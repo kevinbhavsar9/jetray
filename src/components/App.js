9efa77ae5be3074ea8216f159369d596a44fb2f2
kevin bhavsar <kevinbhavsar567@gmail.com>
2021-05-19 20:59:42 +0530

    dispatch and sunscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,46 @@
-import {data} from '../data';
+import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     <Navbar/>
-     <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
+class App extends React.Component {
+  componentDidMount(){
+    const {store} =this.props;
+    // .subscribe() take function as a argument
+    store.subscribe(()=>{
+      console.log('UPDATED');
+      this.forceUpdate();
+    })
+    //make api call
+    //dispatch action
+    store.dispatch({
+      type:'ADD_MOVIES',
+      movies:data
+    });
+    console.log('state',this.props.store.getState())
+  }
+  render() {
+    const movies = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+
+          <div className="List">
+            {movies.map((movie, index) => {
+              return <MovieCard movie={movie} key={`movies-${index}`} />
+            })}
+
+          </div>
         </div>
+      </div>
+    );
+  }
 
-        <div className="List">
-          {data.map(movie=>{
-            return <MovieCard movie={movie} />
-          })}
-
-        </div>
-     </div>
-    </div>
-  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
+
 import './index.css';
 import App from './components/App';
+import movies from './Reducers'
+
+//configured reducer with store
+const store=createStore(movies);
+
+console.log('store',store)
+// console.log('state before',store.getState());
+
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{name:"Superman"}]
+// });
+
+// console.log('state After',store.getState());
+
+ 
 
 
-ReactDOM.render(
+ReactDOM.render( 
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App
+    store={store}
+     />
+  </React.StrictMode>, 
   document.getElementById('root')
 );
 
