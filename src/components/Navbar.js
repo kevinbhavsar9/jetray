c21da55c7a72dfbb6c89c529f0163e4b921516ae
kevin bhavsar <kevinbhavsar567@gmail.com>
2022-07-25 00:41:54 +0530

    ad

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,62 +1,65 @@
 // import { render } from "@testing-library/react";
 import React from "react";
 import { connect } from "../index";
-import { handleMovieSearch,addMovieToList } from '../actions'
-
-
+import { handleMovieSearch, addMovieToList } from "../actions";
 
 class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // showSearchResults: false,
+      searchText: "",
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            // showSearchResults: false,
-            searchText: ''
-        };
-    }
+  handleSearch = () => {
+    const { searchText } = this.state;
+    this.props.dispatch(handleMovieSearch(searchText));
+  };
 
-    handleSearch = () => {
-        const { searchText } = this.state;
-        this.props.dispatch(handleMovieSearch(searchText));
+  handleChange = (e) => {
+    this.setState({
+      searchText: e.target.value,
+    });
+    // console.log(e.target.value)
+  };
+  handleAddToMovies = (movie) => {
+    this.props.dispatch(addMovieToList(movie));
+  };
+  render() {
+    const { result, showSearchResults } = this.props.search;
 
-    }
+    return (
+      <div
+        className="nav"
+        style={{ display: "flex", justifyContent: "space-around" }}
+      >
+        <div style={{ color: "White" }}>
+          <h1>JetRay</h1>
+        </div>
+        <div className="search-container">
+          <input onChange={this.handleChange} />
+          <button id="search-btn" onClick={this.handleSearch}>
+            Search
+          </button>
 
-
-    handleChange = (e) => {
-        this.setState({
-            searchText: e.target.value
-        })
-        // console.log(e.target.value)
-    }
-    handleAddToMovies=(movie)=>{
-        this.props.dispatch(addMovieToList(movie))
-    }
-    render() {
-        const { result,showSearchResults } = this.props.search;
-      
-        return ( 
-            <div className="nav">
-                <div className="search-container">
-                    <input onChange={this.handleChange} />
-                    <button id="search-btn" onClick={this.handleSearch}>Search</button>
-
-                    {showSearchResults &&
-                        <div className="search-results">
-                            <div className="search-result">
-                                <img src={result.Poster } alt="search-pic" />
-                                <div className="movie-info">
-                                    <span>{ result.Title}</span>
-                                    <button onClick={() => this.handleAddToMovies(result)}>Add to Movies</button>
-                                </div>
-                            </div>
-                        </div>
-                    }
-
+          {showSearchResults && (
+            <div className="search-results">
+              <div className="search-result">
+                <img src={result.Poster} alt="search-pic" />
+                <div className="movie-info">
+                  <span>{result.Title}</span>
+                  <button onClick={() => this.handleAddToMovies(result)}>
+                    Add to Movies
+                  </button>
                 </div>
+              </div>
             </div>
-        );
-    }
-
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 // class NavbarWrapper extends React.Component{
@@ -64,16 +67,16 @@ class Navbar extends React.Component {
 //         return (<StoreContext.Consumer>
 //             {(store)=><Navbar dispatch={store.dispatch} search={this.props}/>}
 //         </StoreContext.Consumer>);
-        
+
 //     }
 // }
 
-function mapStoreToProps(state){
-    return{
-        movies:state.movies,
-        search:state.search 
-    }
+function mapStoreToProps(state) {
+  return {
+    movies: state.movies,
+    search: state.search,
+  };
 }
 
-const connectedNavbarComponent=connect(mapStoreToProps)(Navbar);
+const connectedNavbarComponent = connect(mapStoreToProps)(Navbar);
 export default connectedNavbarComponent;
